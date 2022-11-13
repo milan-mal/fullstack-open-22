512581094e2dc595637bdb3285761895891087fb
Milan M <milanmalenak@gmail.com>
2022-11-13 18:27:24 +0100

    Multiple components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,22 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='George' age={26 + 10}/>
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
 
 export default App;
