925b2e020e2129109cf90654dd0f4894c5cf55d3
Milan M <milanmalenak@gmail.com>
2022-12-11 18:24:27 +0100

    refactor the app after changing individual variables to objects

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,5 +1,6 @@
 const Header = (props) => {
   console.log("component \"Header\" run");
+  console.log(props);
   return (
     <h1>{props.course}</h1>
   )
@@ -7,12 +8,13 @@ const Header = (props) => {
 
 
 const Content = (props) => {
-  console.log("component \"Content\" run");
+  console.log("component \"Content2\" run");
+  console.log(props);
   return (
     <div>
-      <Part part={props.part1} exercise={props.exercises1} />
-      <Part part={props.part2} exercise={props.exercises2} />
-      <Part part={props.part3} exercise={props.exercises3} />
+      <Part part={props.part1.name} exercise={props.part1.exercises} />
+      <Part part={props.part2.name} exercise={props.part2.exercises} />
+      <Part part={props.part3.name} exercise={props.part3.exercises} />
     </div>
   )
 }
@@ -20,6 +22,7 @@ const Content = (props) => {
 
 const Part = (props) => {
   console.log("component \"Part\" run");
+  console.log(props);
   return (
     <div>
       <p>{props.part}: {props.exercise} exercises</p>
@@ -30,6 +33,7 @@ const Part = (props) => {
 
 const Total = (props) => {
   console.log("component \"Total\" run");
+  console.log(props);
   return (
     <div>
       <p>Total number of exercises is {props.exercisesTotal}.</p>
@@ -43,19 +47,25 @@ const App = () => {
   console.log("component \"App\" run");
 
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
 
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total exercisesTotal={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total exercisesTotal={part1.exercises + part2.exercises + part3.exercises} />
 
     </div>
   )
