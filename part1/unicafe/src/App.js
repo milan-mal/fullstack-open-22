3c62066cc67001eb27f69c7a7b84827d6174f417
Milan M <milanmalenak@gmail.com>
2022-12-14 17:15:02 +0100

    reinstall modules, 1.6: unicafe step1 finished

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,7 +8,14 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
     </div>
   )
 }
