9f342a9dfd35e5e2f7b88bf92ed7fd40412dc938
Milan M <milanmalenak@gmail.com>
2022-12-17 11:59:35 +0100

    1.8: unicafe step3,refactor statistics to a module

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,21 +1,13 @@
 import { useState } from 'react'
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  let sumFeedb = good + neutral + bad
+const Statistics = ({good, neutral, bad}) => {
+  
+  let sumFeedb = isNaN(good + neutral + bad) ? 0 : good + neutral + bad
   let averageFeedb = (good + neutral + bad) / 3
-  let positiveFeedb = good / sumFeedb * 100
+  let positiveFeedb = sumFeedb == 0 ? 0 : good / sumFeedb * 100
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>statistics</h1>
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
@@ -23,6 +15,23 @@ const App = () => {
       <p>all: {sumFeedb}</p>
       <p>average: {averageFeedb}</p>
       <p>positive: {positiveFeedb} %</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
