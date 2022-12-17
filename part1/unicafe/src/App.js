d5f5d8149049b3a847860e1aad1ebe1d9b166798
Milan M <milanmalenak@gmail.com>
2022-12-17 13:06:07 +0100

    1.10 Extract components Button and StatisticLine

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 
-const Statistics = ({good, neutral, bad}) => {
+const StatisticLine = ({ statType, statValue, statUnit }) => (
+  <div>
+    <p>{statType}: {statValue} {statUnit}</p>
+  </div>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
   
   let sumFeedb = good + neutral + bad
 
@@ -11,18 +17,22 @@ const Statistics = ({good, neutral, bad}) => {
     return (
       <div>
         <h1>statistics</h1>
-        <p>good: {good}</p>
-        <p>neutral: {neutral}</p>
-        <p>bad: {bad}</p>
-        <p>all: {sumFeedb}</p>
-        <p>average: {averageFeedb}</p>
-        <p>positive: {positiveFeedb} %</p>
+        <StatisticLine statType='good' statValue={good} />
+        <StatisticLine statType='neutral' statValue={neutral} />
+        <StatisticLine statType='bad' statValue={bad} />
+        <StatisticLine statType='all' statValue={sumFeedb} />
+        <StatisticLine statType='average' statValue={averageFeedb} />
+        <StatisticLine statType='positive' statValue={positiveFeedb} statUnit='%'/>
       </div>
     )
   }
 
   return <p>No feedback given</p>
 }
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+)
 
 const App = () => {
   // save clicks of each button to its own state
@@ -33,9 +43,9 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
