10a8f76e35945ed3c1ef217868a6f63da8a20244
Milan M <milanmalenak@gmail.com>
2022-12-27 13:47:57 +0100

    2.6: The Phonebook init

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+const App = () => {
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  )
+}
+
+export default App;
