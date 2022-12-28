6e12ca0fab45679c46e94f2a616463947e25cbe8
Milan M <milanmalenak@gmail.com>
2022-12-28 09:35:17 +0100

    2.8: The Phonebook Step3: added phone number

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,10 +4,12 @@ const App = () => {
   const [persons, setPersons] = useState([
     {
       name: 'Arto Hellas',
-      id: 0
+      id: 0,
+      number: '+420 603 521 890',
     },
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setnewNumber] = useState('')
 
   const nameDuplicityCheck = (checkName) => {
     const isDuplicate = persons.some((person) => person.name === checkName)
@@ -17,17 +19,23 @@ const App = () => {
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
+  
+  const handleNumberChange = (event) => {
+    setnewNumber(event.target.value)
+  }
 
   const addPerson = (event) => {
     event.preventDefault()
-    console.log('newName', newName);
+    console.log('newName: ', newName,', newNumber: ', newNumber);
     if (!nameDuplicityCheck(newName)) {
       const newPerson = {
         name: newName,
-        id: persons.length
+        id: persons.length,
+        number: newNumber,
       }
       setPersons(persons.concat(newPerson))
       setNewName('')
+      setnewNumber('')
       return
     }
     alert(`${newName} is already added to phonebook`)
@@ -44,12 +52,18 @@ const App = () => {
           />
         </div>
         <div>
+          number: <input
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+        <div>
           <button type='submit' >add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <p key={person.id}>{person.id}, {person.name}</p>)}
+        {persons.map(person => <p key={person.id}>{person.id} {person.name} {person.number}</p>)}
 
       </div>
     </div>
