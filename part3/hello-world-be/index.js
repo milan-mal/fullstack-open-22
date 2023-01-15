f15414bc29842f48ca520c7ac015f5e06db2ca1f
Milan M <milanmalenak@gmail.com>
2023-01-15 12:52:56 +0100

    part3/a Deleting resources.

--- part3/hello-world-be/index.js
+++ part3/hello-world-be/index.js
@@ -43,6 +43,13 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
