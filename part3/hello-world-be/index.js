4cd2cefcda0d5046d4d5376f7f82547a6a37602e
Milan M <milanmalenak@gmail.com>
2023-01-15 12:26:10 +0100

    part3/a Code-along: REST+Fetching a resource.

--- part3/hello-world-be/index.js
+++ part3/hello-world-be/index.js
@@ -31,6 +31,18 @@ app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
 
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.statusMessage = "Note with this id doesn't exist."
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
