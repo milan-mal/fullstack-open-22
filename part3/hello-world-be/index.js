18f246c07900dd12b7c89c60f20fda041d38f85a
Milan M <milanmalenak@gmail.com>
2023-01-17 17:41:27 +0100

    part 3/a code-along: Receiving data.

--- part3/hello-world-be/index.js
+++ part3/hello-world-be/index.js
@@ -1,6 +1,7 @@
-const { response } = require('express')
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 let notes = [
     {
@@ -48,6 +49,34 @@ app.delete('/api/notes/:id', (request, response) => {
   notes = notes.filter(note => note.id !== id)
 
   response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date (),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  console.log(request.headers)
 })
 
 const PORT = 3001
