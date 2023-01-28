1aaf0d8330ef945d9fc3486499bce4cc56e7e7ae
Milan M <milanmalenak@gmail.com>
2023-01-28 17:27:55 +0100

    Part3-b code-along: build prep.

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   const[notes, setNotes] = useState([])
   const[newNote, setNewNote] = useState('')
   const[showAll, setShowAll] = useState(true)
-  const[errorMessage, setErrorMessage] = useState('some error happened...')
+  const[errorMessage, setErrorMessage] = useState('')
 
   useEffect(() => {
     noteService
--- part2/notes/src/components/Notification.js
+++ part2/notes/src/components/Notification.js
@@ -1,5 +1,5 @@
 const Notification = ({ message }) => {
-    if ( message === null )
+    if ( message === '' )
         return null
     
     return (
