06b53d9c75c132a4d2f019927b8a4fcd3ee0a07e
Milan M <milanmalenak@gmail.com>
2023-01-28 13:12:36 +0100

    Part3 code-along: Cors.

--- part3/hello-world-be/index.js
+++ part3/hello-world-be/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 let notes = [
     {
@@ -89,7 +91,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3002
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
