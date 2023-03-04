ece8aa2f8f6a71debaeeea9015a0d3c9fac9f997
Milan M <milanmalenak@gmail.com>
2023-03-04 16:53:13 +0100

    3c: mongo.js - added fetching notes.

--- part3/notes-be/mongo.js
+++ part3/notes-be/mongo.js
@@ -20,12 +20,19 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-  content: 'HTML is Easy',
-  important: true,
+/* const note = new Note({
+  content: 'I am not sure if I should add a date to the model or not.',
+  important: false,
 })
 
 note.save().then(result => {
   console.log('note saved!')
+  mongoose.connection.close()
+}) */
+
+Note.find({ important: true }).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
   mongoose.connection.close()
 })
