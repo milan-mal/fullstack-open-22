4ce946f589f8f0ab4f7a77b9014be7ad5b8c5f14
Milan M <milanmalenak@gmail.com>
2023-03-07 10:23:11 +0100

    3c: Verifying FE-BE integration, fixed get note.

--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const baseUrl = '/api/notes'
+const baseUrl = 'http://localhost:3001/api/notes'
+/* const baseUrl = '/api/notes' */
 
 const getAll = () => {
     const request = axios.get(baseUrl)
--- part3/notes-be/index.js
+++ part3/notes-be/index.js
@@ -1,55 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
 const app = express()
+
+const Note = require('./models/note')
 
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
 
-const password = process.argv[2]
-
-const url =
-  `mongodb+srv://milanm:${password}@cluster0.biwspvh.mongodb.net/noteApp?retryWrites=true&w=majority`
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2022-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2022-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2022-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]
+let notes = []
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -62,52 +22,40 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    response.json(note)
-  } else {
-    response.statusMessage = "Note with this id doesn't exist."
-    response.status(404).end()
-  }
+  console.log('EP:', request.url)
+  Note.findById(request.params.id)
+    .then(note => {
+      console.log('note', note)
+      response.json(note)
+    })
+    .catch((error) =>{
+      console.log(`Note with this id doesn't exist.`, error.message)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 })
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
-  console.log("body", body);
-  console.log("body.content", body.content);
-  console.log("body", typeof(body));
-  console.log("body.content", typeof(body.content));
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
       error: 'content missing'
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date (),
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 
   console.log(request.headers)
 })
@@ -118,7 +66,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
