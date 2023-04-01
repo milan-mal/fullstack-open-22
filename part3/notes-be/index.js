06b32162e9b6a64575ae6efe01300a6e2d109592
Milan M <milanmalenak@gmail.com>
2023-04-01 22:45:04 +0200

    3c: Moving error handling into middleware.

--- part3/notes-be/index.js
+++ part3/notes-be/index.js
@@ -21,7 +21,7 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   console.log('EP:', request.url)
   Note.findById(request.params.id)
     .then(note => {
@@ -31,11 +31,8 @@ app.get('/api/notes/:id', (request, response) => {
         response.status(404).end()
       }
     })
-    .catch(error => {
-      console.log(error)
-      response.status(400).send({ error: 'malformatted id' })
+    .catch(error => next(error))
   })
-})
 
 app.delete('/api/notes/:id', (request, response) => {
   Note.findById(request.params.id).then(note => {
@@ -69,6 +66,18 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
