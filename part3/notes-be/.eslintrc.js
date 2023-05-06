580df3a71d7c55ffd060c4036a29d3bbcc8cb3e3
Milan M <milanmalenak@gmail.com>
2023-05-06 15:19:56 +0200

    3.21: Deployed, added Lint.

---
+++ part3/notes-be/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "2"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+}
