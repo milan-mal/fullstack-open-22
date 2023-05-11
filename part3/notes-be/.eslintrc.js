3a8343865a0a3ec9d2380a3d32dec2d2c9ff2c71
Milan M <milanmalenak@gmail.com>
2023-05-11 21:28:44 +0200

    Part4: Project structure - changed eslint.

--- part3/notes-be/.eslintrc.js
+++ part3/notes-be/.eslintrc.js
@@ -1,41 +1,41 @@
 /* eslint-disable linebreak-style */
 module.exports = {
-    'env': {
-        'node': true,
-        'commonjs': true,
-        'es2021': true
-    },
-    'extends': 'eslint:recommended',
-    'overrides': [
+  'env': {
+    'node': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'indent': [
+      'warn',
+      2
     ],
-    'parserOptions': {
-        'ecmaVersion': 'latest'
-    },
-    'rules': {
-        'indent': [
-            'warn',
-            4
-        ],
-        'linebreak-style': [
-            'warn',
-            'windows'
-        ],
-        'quotes': [
-            'warn',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ],
-        'eqeqeq': 'error',
-        'no-trailing-spaces': 'error',
-        'object-curly-spacing': [
-            'error', 'always'
-        ],
-        'arrow-spacing': [
-            'error', { 'before': true, 'after': true }
-        ],
-        'no-console': 0
-    }
+    'linebreak-style': [
+      'warn',
+      'windows'
+    ],
+    'quotes': [
+      'warn',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 0
+  }
 }
