823ace762f2bf7d00eefe6ffb6ca21f562d2d726
Milan M <milanmalenak@gmail.com>
2023-05-27 15:01:16 +0200

    4a Project structure: Refactor project structure.

--- part3/notes-be/models/note.js
+++ part3/notes-be/models/note.js
@@ -1,34 +1,20 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to MongoDB..')
-
-mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) =>{
-        console.log('error connecting to MongoDB: ', error.message)
-    })
-
 const noteSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        minLength: 5,
-        required: true,
-    },
-    important: Boolean,
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  important: Boolean,
 })
-    
+
 noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Note', noteSchema)
